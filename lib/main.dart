e469765f7f1687ac8b4ee4dee5504dd974d83c11
Mohd Asif <76173698+mohdasif690@users.noreply.github.com>
2022-03-08 12:42:21 +0530

    changes done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:todo_app/screens/notes_list.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() async {
   await GetStorage.init();
@@ -13,11 +14,42 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    if (kIsWeb) {
+      return const Webpage();
+    } else {
+      return const Apppage();
+    }
+  }
+}
+
+// ignore: camel_case_types
+class Webpage extends StatelessWidget {
+  const Webpage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const NoteList(),
+    );
+  }
+}
+
+class Apppage extends StatelessWidget {
+  const Apppage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const NoteList(),
